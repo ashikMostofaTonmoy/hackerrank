852ba13a3d10adf76f4ccfc8d1afd99b778f07c1
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-05-23 03:38:00 +0600

    Flipping_bits

---
+++ week2/Flipping_bits.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def bit_not(n, numbits=32):
+    # this returns unsign bitss
+    return (1 << numbits) - 1 - n
+def flippingBits(n):
+    # Write your code here
+    return bit_not(n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
