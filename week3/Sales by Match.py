68f16e77391b567d8419439419d7a895935b9512
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-05-31 16:07:48 +0600

    Sales by Match

---
+++ week3/Sales by Match.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n, ar):
+    # Write your code here
+    count_pair = 0
+    for items in Counter(ar).most_common():
+        if items[1]//2 != 0:
+            count_pair += items[1]//2
+
+    return count_pair
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
